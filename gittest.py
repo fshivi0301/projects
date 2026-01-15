dfb0bcac930d82e9191531f4ea9728b2bbdbb1b5
shivi <fshivi0301@gmail.com>
2026-01-15 20:34:05 +0530

    ghfhhghf fbv           readme

--- gittest.py
+++ gittest.py
@@ -1,8 +1,5 @@
-from networkx.algorithms.approximation.traveling_salesman import threshold_accepting_tsp
-
 print("hellllllloo ")
 for i in range(5):
     for j in range(7):
         print ("*",end="")
     print()
-    abc threshold_accepting_tsp()
